35d09aa185c8649e1f02b9ac6a217418af88b5f0
ProgrammerAyaka <148830791+ProgrammerAyaka@users.noreply.github.com>
2024-04-01 20:56:17 +0800

    修改DEBUG

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--h26f--d%l)duvtxg!no$^3mf&x$1&bsw^g-^w&&r&+pw%4yy='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
